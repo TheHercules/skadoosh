5913c65d515e38534210fb94d816dcf5d21808d3
TheHercules <abhijio082@gmail.com>
2016-10-11 19:25:22 +0530

    Update compress.bash

--- compress.bash
+++ compress.bash
@@ -1,47 +1,43 @@
-#!/bin/bash
-
-# Authors - Neil "regalstreak" Agarwal, Harsh "MSF Jarvis" Shandilya, Tarang "DigiGoon" Kagathara
-# 2016
-# This file is used to run skadoo.sh easily. 
-# Can also be configured with a webhook along with automation as regalstreak and msf-jarvis have done.
-
-
-### Manifest Configuration ###
-
-# Name of the ROM. No Spaces Please.
-# Example: CyanogenMod
-
-name=Cyanogenmod
-
-
-# Manifest link. https:// is mandatory.
-# Example: https://github.com/cyanogenmod/android
-
-manifest=https://github.com/CyanogenMod/android
-
-
-# Manifest branch.
-# Example: cm-14.0
-
-branch=cm-13.0
-
-
-### Compression Configuration ###
-
-# Change the value only if you know what these are.
-# Default is true for both.
-
-# compressrepo
-# If true, will compress the .repo folder.
-
-export compressrepo=true
-
-
-# compressnorepo
-# If true, will compress the stuff except for the .repo folder
-export compressnorepo=true
-
-
-### Finally, execute the stuff. ###
-
-/bin/bash skadoo.sh $name $manifest $branch
+ #!/bin/bash
+ 
+ # Authors - Neil "regalstreak" Agarwal, Harsh "MSF Jarvis" Shandilya, Tarang "DigiGoon" Kagathara
+ # 2016
+ # This file is used to run skadoo.sh easily. 
+ # Can also be configured with a webhook along with automation as regalstreak and msf-jarvis have done.
+ 
+ 
+ ### Manifest Configuration ###
+ 
+  # Name of the ROM. No Spaces Please.
+  # Example: CyanogenMod
+  
+ -name=ColorOS
+ +name=CyanogenMod
+  
+  
+  # Manifest link. https:// is mandatory.
+  # Example: https://github.com/cyanogenmod/android
+  
+ -manifest=https://github.com/Qiangong2/patchrom/default.xml
+ +https://github.com/cyanogenmod/android
+  
+  
+  # Manifest branch.
+  # Example: cm-14.0
+  
+ -branch=jellybean42
+ +branch=cm-14.0
+  
+  
+  ### Compression Configuration ###
+ @@ -39,7 +39,7 @@ export compressrepo=true
+  
+  # compressnorepo
+  # If true, will compress the stuff except for the .repo folder
+ -export compressnorepo=true
+ +export compressnorepo=false
+  
+  
+  ### Finally, execute the stuff. ###
+ 
+ /bin/bash skadoo.sh $name $manifest $branch
